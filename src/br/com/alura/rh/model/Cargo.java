d5f63fcfe0a2d59550afa9f971ec8edbbd463a57
Vitor Albano <vitoralbano@gmail.com>
2021-08-07 18:06:03 -0300

    create employee promote service

--- src/br/com/alura/rh/model/Cargo.java
+++ src/br/com/alura/rh/model/Cargo.java
@@ -2,9 +2,30 @@ package br.com.alura.rh.model;
 
 public enum Cargo {
 
-	ASSISTENTE,
-	ANALISTA,
-	ESPECIALISTA,
-	GERENTE;
+	ASSISTENTE {
+		@Override
+		public Cargo proximoCargo() {
+			return ANALISTA;
+		}
+	},
+	ANALISTA {
+		@Override
+		public Cargo proximoCargo() {
+			return ESPECIALISTA;
+		}
+	},
+	ESPECIALISTA {
+		@Override
+		public Cargo proximoCargo() {
+			return GERENTE;
+		}
+	},
+	GERENTE {
+		@Override
+		public Cargo proximoCargo() {
+			return GERENTE;
+		}
+	};
 
+	public abstract Cargo proximoCargo();
 }
---
+++ src/br/com/alura/rh/service/PromocaoService.java
@@ -0,0 +1,20 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.exceptions.ValidacaoException;
+import br.com.alura.rh.model.Cargo;
+import br.com.alura.rh.model.Funcionario;
+
+public class PromocaoService {
+    public void promover(Funcionario funcionario, boolean metaBatida) {
+        if (Cargo.GERENTE == funcionario.getCargo()) {
+            throw new ValidacaoException("Gerentes nao podem ser promovidos!");
+        }
+
+        if (metaBatida == Boolean.FALSE) {
+            throw new ValidacaoException("Funcionario nao bateu a meta.");
+        }
+
+        var novoCargo = funcionario.getCargo().proximoCargo();
+        funcionario.promover(novoCargo);
+    }
+}
